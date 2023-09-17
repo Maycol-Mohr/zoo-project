26f08a4749f0209404ed170d47f2efd416d1eb2e
Maycol Mohr <maycolmohr@yahoo.com.br>
2023-09-17 15:36:16 +0200

    zoo project created

---
+++ src/main/java/com/maycol/br/zooproject/ZooprojectApplication.java
@@ -0,0 +1,13 @@
+package com.maycol.br.zooproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maycol/br/zooproject/ZooprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maycol.br.zooproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
